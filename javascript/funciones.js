d5e00fcb45025d535ecda5193051215bec3d507d
Rodrigo López Ramos <55782190+rodrigolopezramoss@users.noreply.github.com>
2019-10-29 12:07:17 +0100

    Add files via upload

---
+++ javascript/funciones.js
@@ -0,0 +1,13 @@
+
+function mostrar(elemento){
+document.getElementById(elemento).style.visibility="visible"
+}
+function esconder(elemento){
+document.getElementById(elemento).style.visibility="hidden"
+}
+function sonar(musica){
+document.getElementById(musica).play()
+}
+function parar(musica){
+document.getElementById(musica).pause()
+}
